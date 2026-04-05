d76adb1c4f1e8a28b09973263f6fdfb5a584932a
Pradeep-1-source <pradeepshanmugavel01@gmail.com>
2026-04-05 14:09:59 +0530

    Update contact number and map location

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -17,10 +17,10 @@ export default function Contact() {
               </div>
             </div>
             <div className="info-item">
-              <i className='bx bxl-whatsapp'></i>
+              <i className='bx bxs-phone'></i>
               <div>
                 <h3>Contact Number</h3>
-                <p>+91 9962456600</p>
+                <p>+91 9994400124</p>
               </div>
             </div>
             <div className="info-item">
@@ -32,7 +32,7 @@ export default function Contact() {
             </div>
 
             <div className="contact-actions">
-              <a href="tel:+919962456600" className="btn btn-outline">
+              <a href="tel:+919994400124" className="btn btn-outline">
                 <i className='bx bxs-phone-call'></i> Call Us
               </a>
               <a href="https://wa.me/919994400124" target="_blank" className="btn btn-primary" rel="noreferrer">
@@ -43,7 +43,7 @@ export default function Contact() {
 
           <div className="map-container">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.326887556094!2d78.85764091533036!3d11.237255191993278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bab071f111816e1%3A0xa1ea14a51eb8aecd!2sNew%20Bus%20Stand%20Perambalur!5e0!3m2!1sen!2sin!4v1682498523307!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15664.8437!2d78.8785!3d11.234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bab071f111816e1%3A0xa1ea14a51eb8aecd!2sReliance%20Trends!5e0!3m2!1sen!2sin!4v1712316000000!5m2!1sen!2sin"
               width="100%" height="100%" style={{ border: 0 }} allowFullScreen={false} loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"></iframe>
           </div>
